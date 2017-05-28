3d9f01dea43e98655c344338c7ec7b35226a1498
PeymanKhodadust <peyman.khodadust@gmail.com>
2017-05-29 00:16:57 +0200

    added ResultDetailActivity and click event handler to recyclerview viewholder

--- app/src/main/java/com/example/peyman/publictransportationtimetable/ResultAdapter.java
+++ app/src/main/java/com/example/peyman/publictransportationtimetable/ResultAdapter.java
@@ -1,29 +1,29 @@
 package com.example.peyman.publictransportationtimetable;
 
 import android.content.Context;
-import android.nfc.Tag;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by peyman on 26.05.17.
  */
 
 public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultViewHolder> {
-    //private final ListItemClickListener mOnClickListener;
-    private List<String> mList;
+    private JSONArray mResultList;
+    ListItemClickListener mOnClickListener;
 
-    public ResultAdapter(List<String> list/*, ListItemClickListener listener*/) {
-        mList = list;
-        Log.v(this.getClass().toString(), "Date size: " + mList.size());
-        //mOnClickListener = listener;
+    public ResultAdapter(JSONArray resultList, ListItemClickListener listener) {
+        mResultList = resultList;
+        Log.v(this.getClass().toString(), "Date size: " + resultList.length());
+        mOnClickListener = listener;
 
     }
 
@@ -44,13 +44,19 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultView
 
     @Override
     public void onBindViewHolder(ResultViewHolder holder, int position) {
-        holder.bind(position);
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = mResultList.getJSONObject(position);
+        }catch (JSONException je) {
+            je.printStackTrace();
+        }
+        holder.bind(jsonObject);
     }
 
     @Override
     public int getItemCount() {
-        Log.v(this.getClass().toString(), "Date size: " + mList.size());
-        return mList.size();
+        Log.v(this.getClass().toString(), "Date size: " + mResultList.length());
+        return mResultList.length();
     }
 
     public interface ListItemClickListener{
@@ -59,26 +65,41 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultView
 
 
 
-    class ResultViewHolder extends RecyclerView.ViewHolder /*implements View.OnClickListener*/{
+    class ResultViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        TextView listItemResultView;
+        TextView depTime, transfers, arrTime;
 
         public ResultViewHolder(View itemView) {
             super(itemView);
-            listItemResultView = (TextView) itemView.findViewById(R.id.result_item_one);
-            //itemView.setOnClickListener(this);
+            depTime = (TextView) itemView.findViewById(R.id.dep_time);
+            arrTime = (TextView) itemView.findViewById(R.id.arr_time);
+            transfers = (TextView) itemView.findViewById(R.id.transfers);
+            itemView.setOnClickListener(this);
         }
 
-        void bind(int listIndex) {
-            listItemResultView.setText(String.valueOf(mList.get(listIndex)));
+        void bind(JSONObject jsonObject) {
+            String dTime, aTime, trans;
+            dTime = "";
+            aTime = "";
+            trans = "";
+
+            try {
+                dTime = jsonObject.getJSONObject("from").get("departure").toString();
+                aTime = jsonObject.getJSONObject("to").getString("arrival");
+                trans = Integer.toString(jsonObject.getInt("transfers"));
+            }catch (JSONException je){
+                je.printStackTrace();
+            }
+            depTime.setText(dTime);
+            arrTime.setText(aTime);
+            transfers.setText("transfers: " + trans);
         }
 
-        /*
+
         @Override
         public void onClick(View view) {
             int clickedPosition = getAdapterPosition();
             mOnClickListener.onListItemClick(clickedPosition)  ;
         }
-        */
     }
 }
